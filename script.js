ad009a48e1cc504348e141a4619275678d69082a
Tage <05jota@skola.engelholm.se>
2023-10-05 15:14:52 +0200

    check funkar

--- script.js
+++ script.js
@@ -35,20 +35,12 @@ class Square {
         this.y = y;
         this.size = 100;
         this.color = "none";
-        this.draw();
-    }
-
-    draw() {
         this.ctx.strokeRect(this.x * 100, this.y * 100, this.size, this.size);
     }
 
     place(color, squares) {
         if (this.color == "none") {
-            this.ctx.fillStyle = color;
-            this.ctx.beginPath();
-            this.ctx.arc(this.x * 100 + 50, this.y * 100 + 50, this.size / 2, 0, 2 * Math.PI);
-            this.ctx.fill();
-            this.color = color;
+            this.draw(color);
             this.check(squares);
             return true;
         }
@@ -56,9 +48,40 @@ class Square {
             return false;
     }
 
+    draw(color){
+        this.ctx.fillStyle = color;
+        this.ctx.beginPath();
+        this.ctx.arc(this.x * 100 + 50, this.y * 100 + 50, this.size / 2, 0, 2 * Math.PI);
+        this.ctx.fill();
+        this.color = color;
+    }
+
     check(squares) {
         let enemies = this.checkEnemies(squares, this.x, this.y);
-        
+        let turnedEnemies = [];
+        for(let i = 0; i < enemies.length; i++){
+            for(let j = 1; j < 8; j++) {
+                let index = this.x + enemies[i][0] * j + (this.y + enemies[i][1] * j) * 8;
+                if(index < 64 && index >= 0){
+                    let square = squares[index];
+
+                    if(square.color == this.color) {
+                        for(let k = 0; k < turnedEnemies.length; k++){
+                            squares[turnedEnemies[k]].draw(this.color);
+                            return true;
+                        }
+                        break;
+                    }
+                    else if(square.color == this.enemyColor()){
+                        turnedEnemies[j-1] = square.x + square.y * 8;
+                    }
+                    else
+                        break;
+                }
+                
+            }
+        }
+        return false;
     }
 
     checkEnemies(squares, x, y) {
@@ -69,7 +92,7 @@ class Square {
                 let square = x + j + (y + i) * 8;
                 if(square >= 0 && square < 64) {
                     if(squares[square].color == this.enemyColor()){
-                        enemies[index] = square;
+                        enemies[index] = [j, i];
                         index++;
                     }
                 }
@@ -86,6 +109,4 @@ class Square {
         
         return "none"
     }
-
-    
 }
