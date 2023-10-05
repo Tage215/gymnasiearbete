dabfe755c2d4dd064782d2f76e5b8a820f0b164e
Tage <05jota@skola.engelholm.se>
2023-10-05 22:27:18 +0200

    hela spelet funkar

--- script.js
+++ script.js
@@ -1,32 +1,79 @@
 window.onload = function () {
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas.getContext("2d");
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
 
     let squares = [];
-    gridSize = 64;
+    let gridSize = 64;
     for (let i = 0; i < gridSize; i++) {
         squares[i] = new Square(ctx, i % 8, Math.floor(i / 8));
     }
-    squares[27].place("white", squares);
-    squares[28].place("black", squares);
-    squares[35].place("black", squares);
-    squares[36].place("white", squares);
+    squares[27].draw("white", squares);
+    squares[28].draw("black", squares);
+    squares[35].draw("black", squares);
+    squares[36].draw("white", squares);
+
+    let white = 2;
+    let black = 2;
 
     let turn = "black";
     canvas.onclick = function (e) {
         let coordinates = canvas.getBoundingClientRect();
-        x = Math.floor((e.x - coordinates.left - 5) / 100);
-        y = Math.floor((e.y - coordinates.top - 5) / 100);
-        if (squares[x + y * 8].place(turn, squares)) {
-            if (turn == "black")
-                turn = "white";
-            else
-                turn = "black";
-            canvas.style.borderColor = turn;
-        }
+        let x = Math.floor((e.x - coordinates.left - 5) / 100);
+        let y = Math.floor((e.y - coordinates.top - 5) / 100);
+        let turnedEnemies = squares[x + y * 8].check(squares, turn);
+        if (turnedEnemies.length > 0) {
+            for (let i = 0; i < turnedEnemies.length; i++) {
+                squares[turnedEnemies[i]].draw(turn);
+            }
+            squares[x + y * 8].draw(turn);
 
+            white = 0;
+            black = 0;
+            for(let i = 0; i < gridSize; i++){
+                if(squares[i].color == "white")
+                    white++
+                else if(squares[i].color == "black")
+                    black++;
+            }
+            document.getElementById("whiteCount").innerHTML = white;
+            document.getElementById("blackCount").innerHTML = black;
+
+            turn = switchTurn(turn);
+            checkTurnOver(gridSize, squares, turn);
+        }
     }
 }
+
+function checkTurnOver(gridSize, squares, turn) {
+    let temp = "";
+
+    for (let i = 0; i < gridSize; i++) {
+        temp = squares[i].check(squares, turn);
+        if (temp.length > 0) {
+            return;
+        }
+    }
+
+    turn = switchTurn(turn);
+
+    for (let i = 0; i < gridSize; i++) {
+        temp = squares[i].check(squares, turn);
+        if (temp.length > 0) {
+            return;
+        }
+    }
+    gameOver();
+}
+
+function switchTurn(turn) {
+    if (turn == "black")
+        turn = "white";
+    else
+        turn = "black";
+
+    return turn;
+}
+
 
 class Square {
     constructor(ctx, x, y) {
@@ -38,17 +85,7 @@ class Square {
         this.ctx.strokeRect(this.x * 100, this.y * 100, this.size, this.size);
     }
 
-    place(color, squares) {
-        if (this.color == "none") {
-            this.draw(color);
-            this.check(squares);
-            return true;
-        }
-        else
-            return false;
-    }
-
-    draw(color){
+    draw(color) {
         this.ctx.fillStyle = color;
         this.ctx.beginPath();
         this.ctx.arc(this.x * 100 + 50, this.y * 100 + 50, this.size / 2, 0, 2 * Math.PI);
@@ -56,42 +93,50 @@ class Square {
         this.color = color;
     }
 
-    check(squares) {
-        let enemies = this.checkEnemies(squares, this.x, this.y);
+    check(squares, color) {
+        if (this.color != "none")
+            return false;
+        let enemies = this.checkEnemies(squares, color);
         let turnedEnemies = [];
-        for(let i = 0; i < enemies.length; i++){
-            for(let j = 1; j < 8; j++) {
-                let index = this.x + enemies[i][0] * j + (this.y + enemies[i][1] * j) * 8;
-                if(index < 64 && index >= 0){
-                    let square = squares[index];
+        let temp = [];
+        let turnedIndex = 0;
 
-                    if(square.color == this.color) {
-                        for(let k = 0; k < turnedEnemies.length; k++){
-                            squares[turnedEnemies[k]].draw(this.color);
-                            return true;
+        for (let i = 0; i < enemies.length; i++) {
+            temp = [];
+            for (let j = 1; j < 8; j++) {
+                let x = this.x + enemies[i][0] * j;
+                let y = (this.y + enemies[i][1] * j) * 8;
+                if (x < 0 || x > 7)
+                    break;
+                let index = x + y;
+                if (index < 64 && index >= 0) {
+                    let square = squares[index];
+                    if (square.color == color && temp.length > 0) {
+                        for (let k = 0; k < temp.length; k++) {
+                            turnedEnemies[turnedIndex] = temp[k];
+                            turnedIndex++;
                         }
                         break;
                     }
-                    else if(square.color == this.enemyColor()){
-                        turnedEnemies[j-1] = square.x + square.y * 8;
+                    else if (square.color == this.enemyColor(color)) {
+                        temp[j - 1] = square.x + square.y * 8;
                     }
                     else
                         break;
                 }
-                
             }
         }
-        return false;
+        return turnedEnemies;
     }
 
-    checkEnemies(squares, x, y) {
+    checkEnemies(squares, color) {
         let enemies = [];
         let index = 0;
         for (let i = -1; i < 2; i++) {
             for (let j = -1; j < 2; j++) {
-                let square = x + j + (y + i) * 8;
-                if(square >= 0 && square < 64) {
-                    if(squares[square].color == this.enemyColor()){
+                let square = this.x + j + (this.y + i) * 8;
+                if (square >= 0 && square < 64) {
+                    if (squares[square].color == this.enemyColor(color)) {
                         enemies[index] = [j, i];
                         index++;
                     }
@@ -101,12 +146,12 @@ class Square {
         return enemies;
     }
 
-    enemyColor(){
-        if (this.color == "black")
+    enemyColor(color) {
+        if (color == "black")
             return "white";
-        if (this.color == "white")
+        if (color == "white")
             return "black"
-        
+
         return "none"
     }
 }
